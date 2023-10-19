68fb1481bd8f34bb0442d707a335ead2700d0a76
Lei Chen <chen.lei.05@gmail.com>
2023-10-19 17:32:37 -0400

    local_host.py can use two ways (openAI and requests) to POST to vLLM server

--- examples/local_host.py
+++ examples/local_host.py
@@ -1,15 +1,15 @@
 import openai
+from typing import Iterable, List
 
 # Modify OpenAI's API key and API base to use vLLM's API server.
 openai.api_key = "EMPTY"
 # openai.api_base = "http://localhost:8000/v1"
-
 # self-hosted vLLM openAI style server
-openai.api_base = "https://h503711e54cdad8aa-proj-tango-hpc-2.hpc.jpw1a.r-local.net/v1"
+openai.api_base = "https://h273c2bf060bb2192-proj-tango-hpc-2.hpc.jpw1a.r-local.net/v1"
+
 # List models API
 models = openai.Model.list()
 print("Models:", models)
-
 model = models["data"][0]["id"]
 
 # Completion API
@@ -28,3 +28,34 @@ if stream:
         print(c)
 else:
     print(completion)
+
+#vLLM="https://hd01e324754ce48f5-proj-tango-hpc-2.hpc.jpw1a.r-local.net"
+
+vLLM = openai.api_base
+import requests as _requests
+import json
+
+json_body = {
+    "model" : model,
+    "prompt": "A robot may not injure a human being",
+    "n" : 1,
+    "use_beam_search" : False,
+    "logprobs" : 3,
+    }
+
+completion2 = _requests.post(
+    vLLM + "/completions",
+    json = json_body
+    )
+
+def get_response(response: _requests.Response) -> List[str]:
+    data = json.loads(response.content)
+    out_str = json.dumps(data, indent=4)
+    return out_str
+
+print("\n\nCompletion2 results:")
+if stream:
+    for c in completion2:
+        print(c)
+else:
+    print(get_response(completion2))
